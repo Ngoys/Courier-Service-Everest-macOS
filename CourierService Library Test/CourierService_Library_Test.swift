22cf1576910d1c9b8d8cf71d9f4b3e9a4b4d9d4d
Shawn Ngo <shawn@snappymob.com>
2023-03-26 12:38:06 +0800

    [CS] Initial setup commit

---
+++ CourierService Library Test/CourierService_Library_Test.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import CourierService_Library
+
+class CourierService_Library_Test: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(5, my_fun(a: 2, b: 3))
+    }
+}
---
+++ CourierService/main.swift
@@ -0,0 +1,3 @@
+import CourierService_Library
+
+print("3 + 5 = \(my_fun(a: 3, b: 5))")
