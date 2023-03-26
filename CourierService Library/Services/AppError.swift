9732f437b5bf363d9f706dcf0978dccbd828a05d
Shawn Ngo <shawn@snappymob.com>
2023-03-26 21:36:38 +0800

    [CS] Add multiple error enum case for no_of_vehicles max_speed max_carriable_weight

--- CourierService Library/Services/AppError.swift
+++ CourierService Library/Services/AppError.swift
@@ -2,12 +2,15 @@ import Foundation
 
 public enum AppError: Error, Equatable {
     case invalidMenu
-    case invalidBaseFare
+    case invalidBaseDeliveryCost
     case invalidPackageNumber
     case packageNumberLessThan1
     case invalidPackageWithSameID
     case invalidWeightInKG
     case invalidDistanceInKM
+    case numberOfVehiclesLessThan1
+    case invalidMaxSpeed
+    case invalidMaxCarriableWeight
 
     //----------------------------------------
     // MARK: - Properties
@@ -18,23 +21,32 @@ public enum AppError: Error, Equatable {
         case .invalidMenu:
             return "Invalid menu input"
 
-        case .invalidBaseFare:
-            return "Invalid base fare input"
+        case .invalidBaseDeliveryCost:
+            return "Invalid base_delivery_cost input"
 
         case .invalidPackageNumber:
-            return "Invalid number of package"
+            return "Invalid no_of_packages"
 
         case .packageNumberLessThan1:
-            return "Number of package must be more than 0"
+            return "no_of_packages must be more than 0"
 
         case .invalidPackageWithSameID:
             return "Cannot have package with same ID"
 
         case .invalidWeightInKG:
-            return "Invalid weight input, input must be in digit"
+            return "Invalid pkg_weight_in_kg input, input must be in digit"
 
         case .invalidDistanceInKM:
-            return "Invalid distance input, input must be in digit"
+            return "Invalid distance_in_km input, input must be in digit"
+
+        case .numberOfVehiclesLessThan1:
+            return "no_of_vehicles must be more than 0"
+
+        case .invalidMaxSpeed:
+            return "Invalid max_speed input, input must be in digit"
+
+        case .invalidMaxCarriableWeight:
+            return "Invalid max_carriable_weight input, input must be in digit"
         }
     }
 }
--- CourierService/main.swift
+++ CourierService/main.swift
@@ -15,6 +15,8 @@ struct CourierService: ParsableCommand {
     //----------------------------------------
 
     mutating func run() throws {
+        let viewModel = DeliveryViewModel(couponStore: ServiceContainer.container.resolve(CouponStore.self)!)
+
         //----------------------------------------
         // MARK: - Menu Selection
         //----------------------------------------
@@ -26,69 +28,94 @@ struct CourierService: ParsableCommand {
             print("----------------------------------------")
             print("")
 
-            switch menu {
-            case .cost:
-                let viewModel = DeliveryViewModel(couponStore: ServiceContainer.container.resolve(CouponStore.self)!)
+            //----------------------------------------
+            // MARK: - BaseDeliveryCost and NumberOfPackages
+            //----------------------------------------
+            print("Enter input in 'base_delivery_cost no_of_packages' format:")
+            let items = readLine()?.components(separatedBy: " ") ?? []
+            if let baseDeliveryCost = Double(items.first ?? "")?.rounded(toPlaces: 2), baseDeliveryCost >= 0 {
+                print()
 
                 //----------------------------------------
-                // MARK: - Base Fare
+                // MARK: - Package Number
                 //----------------------------------------
-                print("Enter input in 'base_delivery_cost no_of_packages' format:")
-                let items = readLine()?.components(separatedBy: " ") ?? []
-                if let baseDeliveryCost = Double(items.first ?? "")?.rounded(toPlaces: 2), baseDeliveryCost >= 0 {
-                    print()
+                if items.indices.contains(1), let numberOfPackages = Int(items[1]) {
+                    if numberOfPackages > 0 {
 
-                    //----------------------------------------
-                    // MARK: - Package Number
-                    //----------------------------------------
-                    if items.indices.contains(1), let numberOfPackages = Int(items[1]) {
-                        if numberOfPackages > 0 {
+                        //----------------------------------------
+                        // MARK: - Packages Detail
+                        //----------------------------------------
+                        var count = viewModel.getPackages().count
+                        while count < numberOfPackages {
+                            print("Enter \((count + 1).ordinal ?? "") packages details in 'pkg_id pkg_weight_in_kg distance_in_km offer_code' format:")
 
-                            //----------------------------------------
-                            // MARK: - Packages Detail
-                            //----------------------------------------
-                            var count = viewModel.getPackages().count
-                            while count < numberOfPackages {
-                                print("Enter \((count + 1).ordinal ?? "") packages details in 'pkg_id pkg_weight_in_kg distance_in_km offer_code' format:")
+                            let readLine = readLine()
 
-                                let readLine = readLine()
-
-                                do {
-                                    try viewModel.addPackage(text: readLine)
-                                    count += 1
-                                } catch {
-                                    if let appError = error as? AppError {
-                                        print(appError.errorDescription)
-                                        print()
-                                    }
-                                    continue
+                            do {
+                                try viewModel.addPackage(text: readLine)
+                                count += 1
+                            } catch {
+                                if let appError = error as? AppError {
+                                    print(appError.errorDescription)
+                                    print()
                                 }
-
-                                print()
+                                continue
                             }
 
+                            print()
+                        }
+
+                        switch menu {
+                        case .cost:
                             print("----------------------------------------")
                             print("Answer")
                             print("----------------------------------------")
                             viewModel.getPackages().forEach { package in
                                 print("\(package.id) \(viewModel.calculateDiscountedDeliveryCost(baseDeliveryCost: baseDeliveryCost, package: package).removeZerosFromEnd()) \(viewModel.calculateFinalDeliveryCost(baseDeliveryCost: baseDeliveryCost, package: package).removeZerosFromEnd())")
                             }
-                            
-                        } else {
-                            print(AppError.packageNumberLessThan1.errorDescription)
-                            throw AppError.packageNumberLessThan1
+
+                        case .time:
+
+                            //----------------------------------------
+                            // MARK: - NumberOfVehicles and MaxSpeed and MaxCarriableWeight
+                            //----------------------------------------
+                            var isAcceptingInput = true
+                            while isAcceptingInput {
+                                print("Enter input in 'no_of_vehicles max_speed max_carriable_weight' format:")
+
+                                let items = readLine()?.components(separatedBy: " ") ?? []
+                                if let numberOfVehicles = Int(items.first ?? ""), numberOfVehicles >= 0 {
+                                    if items.indices.contains(1), let maxSpeed = Double(items[1]) {
+                                        if items.indices.contains(2), let maxCarriableWeight = Double(items[2]) {
+                                            isAcceptingInput = false
+
+
+
+
+
+                                            
+                                        } else {
+                                            print(AppError.invalidMaxCarriableWeight.errorDescription)
+                                        }
+                                    } else {
+                                        print(AppError.invalidMaxSpeed.errorDescription)
+                                    }
+                                } else {
+                                    print(AppError.numberOfVehiclesLessThan1.errorDescription)
+                                }
+                            }
                         }
                     } else {
-                        print(AppError.invalidPackageNumber.errorDescription)
-                        throw AppError.invalidPackageNumber
+                        print(AppError.packageNumberLessThan1.errorDescription)
+                        throw AppError.packageNumberLessThan1
                     }
                 } else {
-                    print(AppError.invalidBaseFare.errorDescription)
-                    throw AppError.invalidBaseFare
+                    print(AppError.invalidPackageNumber.errorDescription)
+                    throw AppError.invalidPackageNumber
                 }
-
-            case .time:
-                fatalError("Shawn")
+            } else {
+                print(AppError.invalidBaseDeliveryCost.errorDescription)
+                throw AppError.invalidBaseDeliveryCost
             }
         } else {
             print(AppError.invalidMenu.errorDescription)
