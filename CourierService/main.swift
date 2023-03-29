f0eda58dd2706f8183cd3da835018289faf5af4b
Shawn Ngo <shawn@snappymob.com>
2023-03-29 13:26:20 +0800

    [CS] Use guard to prevent too many alignment spaces for if else

--- CourierService/main.swift
+++ CourierService/main.swift
@@ -3,7 +3,7 @@ import Foundation
 import CourierService_Library
 
 struct CourierService: ParsableCommand {
-    //shawn PR merge develop > main branch
+    
     //----------------------------------------
     // MARK: - Properties
     //----------------------------------------
@@ -21,105 +21,104 @@ struct CourierService: ParsableCommand {
         // MARK: - Menu Selection
         //----------------------------------------
         print("Menu option ('cost' or 'time'):", terminator: " ")
-        if let menuString = readLine(), let menu = Menu(rawValue: menuString) {
-            print("")
-            print("----------------------------------------")
-            print("Calculating \(menu.name)! for the packages delivery")
-            print("----------------------------------------")
-            print("")
-
-            //----------------------------------------
-            // MARK: - BaseDeliveryCost and NumberOfPackages
-            //----------------------------------------
-            print("Enter input in 'base_delivery_cost no_of_packages' format:")
-            let items = readLine()?.components(separatedBy: " ") ?? []
-            if let baseDeliveryCost = Double(items.first ?? "")?.rounded(toPlaces: 2), baseDeliveryCost >= 0 {
-                print()
-
-                //----------------------------------------
-                // MARK: - Package Number
-                //----------------------------------------
-                if items.indices.contains(1), let numberOfPackages = Int(items[1]) {
-                    if numberOfPackages > 0 {
-
-                        //----------------------------------------
-                        // MARK: - Packages Detail
-                        //----------------------------------------
-                        var count = viewModel.getPackages().count
-                        while count < numberOfPackages {
-                            print("Enter \((count + 1).ordinal ?? "") packages details in 'pkg_id pkg_weight_in_kg distance_in_km offer_code' format:")
-
-                            let readLine = readLine()
-
-                            do {
-                                try viewModel.addPackage(text: readLine)
-                                count += 1
-                            } catch {
-                                if let appError = error as? AppError {
-                                    print(appError.errorDescription)
-                                    print()
-                                }
-                                continue
-                            }
-
-                            print()
-                        }
-
-                        switch menu {
-                        case .cost:
-                            print("----------------------------------------")
-                            print("Answer")
-                            print("----------------------------------------")
-                            viewModel.getPackages().forEach { package in
-                                print("\(package.id) \(viewModel.calculateDiscountedDeliveryCost(baseDeliveryCost: baseDeliveryCost, package: package).removeZerosFromEnd()) \(viewModel.calculateFinalDeliveryCost(baseDeliveryCost: baseDeliveryCost, package: package).removeZerosFromEnd())")
-                            }
-
-                        case .time:
-
-                            //----------------------------------------
-                            // MARK: - NumberOfVehicles and MaxSpeed and MaxCarriableWeight
-                            //----------------------------------------
-                            var isAcceptingInput = true
-                            while isAcceptingInput {
-                                print("Enter input in 'no_of_vehicles max_speed max_carriable_weight' format:")
-
-                                let items = readLine()?.components(separatedBy: " ") ?? []
-                                if let numberOfVehicles = Int(items.first ?? ""), numberOfVehicles >= 0 {
-                                    if items.indices.contains(1), let maxSpeed = Double(items[1]) {
-                                        if items.indices.contains(2), let maxCarriableWeight = Double(items[2]) {
-                                            isAcceptingInput = false
-
-
-
-
-
-
-                                        } else {
-                                            print(AppError.invalidMaxCarriableWeight.errorDescription)
-                                        }
-                                    } else {
-                                        print(AppError.invalidMaxSpeed.errorDescription)
-                                    }
-                                } else {
-                                    print(AppError.numberOfVehiclesLessThan1.errorDescription)
-                                }
-                            }
-                        }
-                    } else {
-                        print(AppError.packageNumberLessThan1.errorDescription)
-                        throw AppError.packageNumberLessThan1
-                    }
-                } else {
-                    print(AppError.invalidPackageNumber.errorDescription)
-                    throw AppError.invalidPackageNumber
-                }
-            } else {
-                print(AppError.invalidBaseDeliveryCost.errorDescription)
-                throw AppError.invalidBaseDeliveryCost
-            }
-        } else {
+        guard let menuString = readLine(), let menu = Menu(rawValue: menuString) else {
             print(AppError.invalidMenu.errorDescription)
             throw AppError.invalidMenu
+        }
+
+        print("")
+        print("----------------------------------------")
+        print("Calculating \(menu.name)! for the packages delivery")
+        print("----------------------------------------")
+        print("")
+
+        //----------------------------------------
+        // MARK: - BaseDeliveryCost and NumberOfPackages
+        //----------------------------------------
+        print("Enter input in 'base_delivery_cost no_of_packages' format:")
+        let items = readLine()?.components(separatedBy: " ") ?? []
+        guard let baseDeliveryCost = Double(items.first ?? "")?.rounded(toPlaces: 2), baseDeliveryCost >= 0 else {
+            print(AppError.invalidBaseDeliveryCost.errorDescription)
+            throw AppError.invalidBaseDeliveryCost
+        }
+
+        print()
+
+        //----------------------------------------
+        // MARK: - Package Number
+        //----------------------------------------
+        guard items.indices.contains(1), let numberOfPackages = Int(items[1]) else {
+            print(AppError.invalidPackageNumber.errorDescription)
+            throw AppError.invalidPackageNumber
+        }
+
+        guard numberOfPackages > 0 else {
+            print(AppError.packageNumberLessThan1.errorDescription)
+            throw AppError.packageNumberLessThan1
+        }
+
+        //----------------------------------------
+        // MARK: - Packages Detail
+        //----------------------------------------
+        var count = viewModel.getPackages().count
+        while count < numberOfPackages {
+            print("Enter \((count + 1).ordinal ?? "") packages details in 'pkg_id pkg_weight_in_kg distance_in_km offer_code' format:")
+
+            let readLine = readLine()
+
+            do {
+                try viewModel.addPackage(text: readLine)
+                count += 1
+            } catch {
+                if let appError = error as? AppError {
+                    print(appError.errorDescription)
+                    print()
+                }
+                continue
+            }
+
+            print()
+        }
+
+        switch menu {
+        case .cost:
+            print("----------------------------------------")
+            print("Answer")
+            print("----------------------------------------")
+            viewModel.getPackages().forEach { package in
+                print("\(package.id) \(viewModel.calculateDiscountedDeliveryCost(baseDeliveryCost: baseDeliveryCost, package: package).removeZerosFromEnd()) \(viewModel.calculateFinalDeliveryCost(baseDeliveryCost: baseDeliveryCost, package: package).removeZerosFromEnd())")
+            }
+
+        case .time:
+
+            //----------------------------------------
+            // MARK: - NumberOfVehicles and MaxSpeed and MaxCarriableWeight
+            //----------------------------------------
+            var isAcceptingInput = true
+            while isAcceptingInput {
+                print("Enter input in 'no_of_vehicles max_speed max_carriable_weight' format:")
+
+                let items = readLine()?.components(separatedBy: " ") ?? []
+                if let numberOfVehicles = Int(items.first ?? ""), numberOfVehicles >= 0 {
+                    if items.indices.contains(1), let maxSpeed = Double(items[1]) {
+                        if items.indices.contains(2), let maxCarriableWeight = Double(items[2]) {
+                            isAcceptingInput = false
+
+
+
+
+
+
+                        } else {
+                            print(AppError.invalidMaxCarriableWeight.errorDescription)
+                        }
+                    } else {
+                        print(AppError.invalidMaxSpeed.errorDescription)
+                    }
+                } else {
+                    print(AppError.numberOfVehiclesLessThan1.errorDescription)
+                }
+            }
         }
     }
 }
